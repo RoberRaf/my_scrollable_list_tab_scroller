b62a074c3bcda812d979ee5de53551702f1a3636
RoberRaf <135549922+RoberRaf@users.noreply.github.com>
2023-12-10 23:37:21 +0200

    Add files via upload

---
+++ test/my_scrollable_list_tab_scroller_test.dart
@@ -0,0 +1,10 @@
+import 'package:my_scrollable_list_tab_scroller/my_scrollable_list_tab_scroller.dart';
+
+void main() {
+  // test('adds one to input values', () {
+  //   final calculator = Calculator();
+  //   expect(calculator.addOne(2), 3);
+  //   expect(calculator.addOne(-7), -6);
+  //   expect(calculator.addOne(0), 1);
+  // });
+}
